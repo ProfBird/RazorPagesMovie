f3f4aaa16deadf7db1ca72f0d9eaadb4ad4f8307
Brian Bird <profbird@gmail.com>
2017-10-26 17:37:34 -0700

    Mac Version works!

--- RazorPagesMovie.Mac/Startup.cs
+++ RazorPagesMovie.Mac/Startup.cs
@@ -1,11 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
+using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie.Mac
 {
@@ -21,6 +20,10 @@ namespace RazorPagesMovie.Mac
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.AddDbContext<MovieContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("MovieContext")));
             services.AddMvc();
         }
 
