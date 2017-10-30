9fc7ed6eab1f1c9e1fea146b5acb84a56f4a8154
Brian Bird <profbird@gmail.com>
2017-10-29 17:42:19 -0700

    Added Genre drop-down selection for search

--- RazorPagesMovie.Mac/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie.Mac/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
 
@@ -19,17 +20,24 @@ namespace RazorPagesMovie.Mac.Pages.Movies
         }
 
         public IList<Movie> Movie { get;set; }
-
+        public SelectList Genres;
+        public string MovieGenre { get; set; }
+        
         /*
         public async Task OnGetAsync()
         {
             Movie = await _context.Movie.ToListAsync();
         }
         */
-        
+
         // Search for a movie by title
-        public async Task OnGetAsync(string searchString)
+        public async Task OnGetAsync(string movieGenre, string searchString)
         {
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                    orderby m.Genre
+                                    select m.Genre;
+
             var movies = from m in _context.Movie
                         select m;
 
@@ -37,6 +45,13 @@ namespace RazorPagesMovie.Mac.Pages.Movies
             {
                 movies = movies.Where(s => s.Title.Contains(searchString));
             }
+
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+
             // Return the movie with the specified title, or 
             // all movies in the database if no title was specified
             Movie = await movies.ToListAsync();
